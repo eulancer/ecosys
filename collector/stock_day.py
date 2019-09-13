03e0cec7bac0eca15d0c6756bfc5e4c1aa074552
lancer <tokttt@gmail.com>
2019-09-13 21:14:01 +0800

    更新了下载所有数据

--- collector/stock_day.py
+++ collector/stock_day.py
@@ -3,6 +3,8 @@ import tushare
 import pandas as pd
 from tushare_util import get_pro_client
 
+import config
+
 
 def get_stock_list():
     pro = get_pro_client()
@@ -12,45 +14,26 @@ def get_stock_list():
     return data
 
 
-def get_stock_hist_data(code, start='2010-01-01',
-                        end=time.strftime('%Y-%m-%d', time.localtime())):
-    # 获取股票历史数据
-    # 调用tushare的get_hist_data()方法
-    # 从中选取如下返回值：
-    # date: 日期
-    # code: 股票代码
-    # open: 开盘价
-    # high: 最高价
-    # close: 收盘价
-    # low: 最低价
+def get_stock_hist_data(code, start, end):
     pro = get_pro_client()
-    data = pro.get_hist_data(code, start, end)
+    start = start
+    end = end
+    data = pro.daily(ts_code=code, start_date=start, end_date=end)
     stock_data = pd.DataFrame(data)
-
     stock_data.to_sql(name="stock_his_data", con=config.engine, schema="test", index=True, if_exists='append',
                       chunksize=1000)
-    """ 
-    # 存放数据的列表
-    stock_list = []
-    # 组合所需元素(日期-股票代码-开盘价-收盘价-最高价-最低价)
-    temp = zip(data.index, [code for x in range(len(data.index))], data.open,
-               data.close, data.high, data.low)
-    for i in temp:
-        stock_list.append(i)
-    # 默认股票数据是最新日期在前的，想按日期生序排列，倒着返回
-    """
+    print(code + "已存入mysql")
 
 
 def main():
-    stock_list = get_stock_list()
-    print(stock_list)
-    pro = get_pro_client()
-    alldays = pro.trade_cal()
-    print(alldays)
-    #for row in stock_list.iterrows():
-        #print(row["symbol"][0])
-        # get_stock_hist_data(row["symbol"].values)
-    # get_stock_hist_data()
+    stock_list = pd.DataFrame(get_stock_list())
+    start = '20010101'
+    end = time.strftime('%Y%m%d', time.localtime())
+    print(end)
+    for index, row in stock_list.iterrows():
+        get_stock_hist_data(row["ts_code"], start, end)
+        time.sleep(0.005)
+    print("下载结束")
 
 
 if __name__ == '__main__':
