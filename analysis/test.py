9463d6f809f5211dfb2ea77d5720d920be8fa6eb
lancer <tokttt@gmail.com>
2019-09-23 22:03:35 +0800

    新增预增指标筛选

--- analysis/test.py
+++ analysis/test.py
@@ -18,23 +18,26 @@ def increase_stocks(current_day, last_day):
     # df1 = df1.append(df1h)
     # dft = dft.append(dfth)
     print(df1)
-    # stock列表
+    df2 = pro.stock_basic()
+    df2 = df2[~df2.name.str.contains('ST')]
+    data_list = df2['ts_code'].values.tolist()
+
     stockList = pd.DataFrame(columns=df1.columns)
     stockList["rate"] = None
     rates = []
-    stockList_noST=stcok_list_nost()
     for index, row in df1.iterrows():
         if len(row) > 0:
             ts_codeT = row["ts_code"]
-            if len(dft[dft.ts_code == ts_codeT]["vol"]) > 0 and ts_codeT in stockList_noST:
-                vol1 = row["vol"]
-                vol2 = dft[dft.ts_code == ts_codeT]["vol"].values[0]
-                rate = vol1 / vol2
-                if rate > 4 and row["pct_chg"] > 0:
-                    print(rate)
-                    stockList = stockList.append(row)
-                    rates.append(rate)
-                    stockList["rate"] = rates
+            if ts_codeT in data_list:
+                if len(dft[dft.ts_code == ts_codeT]["vol"]) > 0:
+                    vol1 = row["vol"]
+                    vol2 = dft[dft.ts_code == ts_codeT]["vol"].values[0]
+                    rate = vol1 / vol2
+                    if rate > 4 and row["pct_chg"] > 0:
+                        print(rate)
+                        stockList = stockList.append(row)
+                        rates.append(rate)
+                        stockList["rate"] = rates
     print(stockList)
     # 导出为csv
     stockList.to_csv("std.csv", encoding="gbk", index=False)
@@ -45,7 +48,7 @@ def increase_stocks(current_day, last_day):
 
 # 获取两次放量数据
 def check_twice_increase():
-    sql = "select* from  stock_history where ts_code in (select ts_code from stock_history group by ts_code having " +\
+    sql = "select* from  stock_history where ts_code in (select ts_code from stock_history group by ts_code having " + \
           "count(ts_code) > 1) "
     df = pd.read_sql(sql=sql, con=config.engine)
     print("两次放量")
@@ -78,20 +81,11 @@ def get_recent_two_tradingdates():
     recent_two_tradingdates = [last_day, last_second_day]
     return recent_two_tradingdates
 
-def stcok_list_nost():
-    pro = get_pro_client()
-    df2 = pro.stock_basic()
-    df2 = df2[~df2.name.str.contains('ST')]
-    data_list = df2['ts_code'].values.tolist()
-    return data_list
-
 
 def main():
     # 获取最近2个交易日
     days = get_recent_two_tradingdates()
-    print(days)
     increase_stocks(days[0], days[1])
-    check_twice_increase()
 
 
 if __name__ == "__main__":
